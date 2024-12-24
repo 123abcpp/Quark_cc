0dbd0c8613615eec17c1413d7165c4e1548f9f36
Yiliang Dong <dongyiliangsteven@163.com>
2024-12-24 20:57:52 +0000

    fix cc writeback

--- qlib/kernel/fs/host/hostinodeop.rs
+++ qlib/kernel/fs/host/hostinodeop.rs
@@ -1005,7 +1005,9 @@ impl HostInodeOp {
         };
 
         if is_cc_active(){
-            self.lock().Mappable().lock().WritebackAllPages();
+            if self.lock().mappable.is_some() {
+                self.lock().Mappable().lock().WritebackAllPages();
+            }
         }
 
         let ret = if SHARESPACE.config.read().UringIO && self.InodeType() == InodeType::RegularFile
